c6e2e957222f8cdc5219a3067701af5e4556392f
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-05-20 10:59:56 +0500

    integrated components into App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
+import React, { useState } from "react";
+import './App.css'
+import CourseInput from "./components/CourseInput/CourseInput";
+import GoalList from "./components/GoalList/GoalList";
 
 function App() {
+  const [courseGoals, setCourseGoals] = useState([
+    { text: 'Do all exercises!', id: 'g1' },
+    { text: 'Finish the course!', id: 'g2' }
+  ]);
+
+  const handleGoalAdd = enteredText => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  const handleItemDelete = goalId => {
+    setCourseGoals(prevGoals => {
+      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
+      console.log(prevGoals)
+      return updatedGoals;
+    });
+  };
+
+  let content = (
+    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+  );
+
+  if (courseGoals.length > 0) {
+    content = (
+      <GoalList items={courseGoals} onDeleteItem={handleItemDelete} />
+    );
+  }
   return (
     <div className="App">
-      
+      <section id="goal-form">
+        <CourseInput onAddGoal={handleGoalAdd}/>
+      </section>
+
+      <section id="goals">
+        {content}
+      </section>      
     </div>
   );
 }
